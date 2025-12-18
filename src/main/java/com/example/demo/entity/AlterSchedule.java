7bbffa6ca9dd80980553e2727ec788b3a420f9d5
bala-amypo <bala@amypo.in>
2025-12-18 13:40:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlterSchedule.java
+++ src/main/java/com/example/demo/entity/AlterSchedule.java
@@ -1,6 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
