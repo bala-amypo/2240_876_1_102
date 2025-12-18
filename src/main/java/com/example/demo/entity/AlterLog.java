4de7e2b1818d6f3057fd4cc1226faabcf12ac2b9
bala-amypo <bala@amypo.in>
2025-12-18 10:41:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlterLog.java
+++ src/main/java/com/example/demo/entity/AlterLog.java
@@ -15,34 +15,5 @@ public class AlterLog {
     private ManyToOne warranty;
     private LocalDateTime sentAt;
     private String message;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public ManyToOne getWarranty() {
-        return warranty;
-    }
-    public void setWarranty(ManyToOne warranty) {
-        this.warranty = warranty;
-    }
-    public LocalDateTime getSentAt() {
-        return sentAt;
-    }
-    public void setSentAt(LocalDateTime sentAt) {
-        this.sentAt = sentAt;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public AlterLog(ManyToOne warranty, LocalDateTime sentAt, String message) {
-        this.warranty = warranty;
-        this.sentAt = sentAt;
-        this.message = message;
-    }
 
 }
--- src/main/java/com/example/demo/entity/AlterSchedule.java
+++ src/main/java/com/example/demo/entity/AlterSchedule.java
@@ -1,6 +1,11 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 public class AlterSchedule {
     @Id
@@ -9,33 +14,5 @@ public class AlterSchedule {
     private ManyToOne warranty;
     private int daysBeforeExpiry;
     private Boolean enabled;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public ManyToOne getWarranty() {
-        return warranty;
-    }
-    public void setWarranty(ManyToMany warranty) {
-        this.warranty = (ManyToOne) warranty;
-    }
-    public int getDaysBeforeExpiry() {
-        return daysBeforeExpiry;
-    }
-    public void setDaysBeforeExpiry(int daysBeforeExpiry) {
-        this.daysBeforeExpiry = daysBeforeExpiry;
-    }
-    public Boolean getEnabled() {
-        return enabled;
-    }
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-    public AlterSchedule(ManyToOne warranty, int daysBeforeExpiry, Boolean enabled) {
-        this.warranty = warranty;
-        this.daysBeforeExpiry = daysBeforeExpiry;
-        this.enabled = enabled;
-    }
+    
 }
