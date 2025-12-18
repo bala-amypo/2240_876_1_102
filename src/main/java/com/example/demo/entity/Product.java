f44989620d12d48716619016fa3d7c72b07ef15a
bala-amypo <bala@amypo.in>
2025-12-18 05:26:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -4,5 +4,25 @@ import jakarta.presistence.*;
 
 public class Product{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique=true)
+    private String email;
+    private String modelNumber;
+    private String category;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public 
 }
