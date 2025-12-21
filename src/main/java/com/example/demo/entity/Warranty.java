2d7c637640b51da6df3ba9d44a452fe156fdfd78
bala-amypo <bala@amypo.in>
2025-12-21 12:30:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -31,9 +31,9 @@ public class Warranty {
     @Column(unique = true)
     private String serialNumber;
 
-    @OneToMany(mappedBy = "warranty")
-    private List<AlertSchedule> schedules;
+    // @OneToMany(mappedBy = "warranty")
+    // private List<AlertSchedule> schedules;
 
-    @OneToMany(mappedBy = "warranty")
-    private List<AlertLog> logs;
+    // @OneToMany(mappedBy = "warranty")
+    // private List<AlertLog> logs;
 }
